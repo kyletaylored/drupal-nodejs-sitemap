f812a5660e141ad2925fad56832f4b67372d7e59
Kyle Taylor <kyletaylored@gmail.com>
2018-06-22 11:48:06 -0500

    Update sitemap with prompts and content audit
    - Added back form extraction
    - Added URL prompt
    - Added progress bar

--- sitemap.js
+++ sitemap.js
@@ -1,39 +1,60 @@
+const prompt = require('prompt')
 const cheerio = require('cheerio')
 const Sitemapper = require('sitemapper')
 const fetch = require('node-fetch')
 const jsonfile = require('jsonfile')
+const cliProgress = require('cli-progress')
 
 let sitemap = new Sitemapper()
-let pagetypes = {}
-let statusCodes = {}
+let bar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic)
 let file = './results/sitemap-results.json'
 let log = console.log.bind(this)
 
-async function asyncForEach (array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array)
-  }
-}
+// Docstore.
+let nodeTypes = {}
+let formTypes = {}
+let statusCodes = {}
 
-async function main () {
+// Create prompt input.
+var properties = [
+  {
+    name: 'url',
+    validator: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)?/gi,
+    warning: 'URL must include HTTP or HTTPS.'
+  }
+]
+
+// Main function to run.
+async function main (sitemapUrl) {
   try {
-    let sites = await sitemap.fetch('https://www.macouncil.org/sitemap.xml')
+    let sites = await sitemap.fetch(sitemapUrl)
+    bar.start(sites.sites.length, 0)
     await asyncForEach(sites.sites, async url => processSitemapPage(url))
   } catch (err) {
     console.log('Err: ', err)
   }
 
-  await log({ 'Page Types': pagetypes, statusCodes: statusCodes })
+  await bar.stop()
+  await log({ 'Node Types': nodeTypes, statusCodes: statusCodes })
   await jsonfile.writeFile(
     file,
     {
-      nodes: pagetypes,
+      nodes: nodeTypes,
+      forms: formTypes,
       statusCodes: statusCodes
     },
     err => console.error(err)
   )
 }
 
+// Helper function for async processing in a for loop.
+async function asyncForEach (array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+
+// Process the URL in the sitemap, store results in docstore.
 async function processSitemapPage (url) {
   await fetch(url)
     .then(resp =>
@@ -43,7 +64,9 @@ async function processSitemapPage (url) {
           let $ = cheerio.load(body)
           // Extract from body.
           let htmlBody = $('body')
-          extractNodeTypes(htmlBody, url, pagetypes)
+          let forms = $('form', htmlBody)
+          extractNodeTypes(htmlBody, url, nodeTypes)
+          extractFormTypes(forms, url, formTypes)
         } else {
           // TODO: this never gets called...
           log('STATUS: ', resp.status)
@@ -52,6 +75,9 @@ async function processSitemapPage (url) {
       })
     )
     .catch(err => console.log(err))
+
+  // Update the current value of progress by 1, even if request fails.
+  bar.increment()
 }
 
 /**
@@ -63,6 +89,17 @@ async function extractNodeTypes (body, url, docstore) {
     if (className.includes('node-type-')) {
       storeResults(docstore, className.substr(10), url)
       break
+    }
+  }
+}
+
+/**
+ * Extract form data.
+ */
+async function extractFormTypes (forms, url, docstore) {
+  if (forms.length > 0) {
+    for (var i = 0; i < forms.length; i++) {
+      storeResults(docstore, forms[i].attribs.id, url)
     }
   }
 }
@@ -85,4 +122,20 @@ function storeResults (docstore, name, url) {
   docstore[name].urls.push(url)
 }
 
-main()
+// Launch script.
+// Start capturing input.
+prompt.start()
+prompt.get(properties, function (err, result) {
+  if (err) {
+    return onErr(err)
+  }
+  console.log('Command-line input received:')
+  // console.log('  URL: ' + result.url)
+  console.tabple(result)
+
+  // Use URL as part of JSON file name (in case running multiple scans.)
+  file = './results/' + result.url.split('.')[1] + '-results.json'
+
+  // Parse sitemap.
+  main(result.url)
+})
